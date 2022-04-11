81748b202fb13fdc2d1ca159f9b5151534f8f323
SokolovAN <sokolovan@infotecs.ru>
2022-04-11 22:49:14 +0300

    Реализация задачи "Пишем свой “Hello, World”. Учимся собирать простые проекты

---
+++ hello_world.cpp
@@ -0,0 +1,14 @@
+// TODO: сначала идет вызов системных хедаров, затем пользовательских
+#include <iostream>
+
+#include "example.h"
+#include "move_me_out.h"
+
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+
+	example_function();
+	todo_move_me_out();
+	
+	return 0;
+}
